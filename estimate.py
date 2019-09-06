f69c6b1fff868b3fa74bf54eca9c55b11abaa690
WANG-Penghui-CEA <53853621+CEA-WANG-Penghui@users.noreply.github.com>
2019-09-06 17:50:36 +0800

    Add files via upload

--- estimate.py
+++ estimate.py
@@ -34,7 +34,7 @@ def readfile():
         datastream.remove(tr)
     datastream.merge()
     outfile = input("output file (support png, pdf, ps, eps and svg):\n")
-    datastream.plot(size = (1200,600), outfile=outfile)
+    datastream.plot(dpi=300,outfile=outfile)
     # mypath: "C:\Users\ironman\Desktop\idea\论文\使用小波分解重构法计算地震计安装方位角\datastream.svg"
     print(datastream,"\n")
     return datastream
@@ -180,33 +180,41 @@ def plothist(ca,sa):
     ca_sigma = np.std(ca, ddof =1)
     sa_mu = np.mean(sa)
     sa_sigma = np.std(sa, ddof =1)
-    plt.subplot(211)
-    n, bins, patches = plt.hist(ca, 1000, range=(ca_mu-3*ca_sigma,ca_mu+3*ca_sigma),color = 'g',alpha=0.75)
-    plt.title('Parameter Ca')
-    plt.subplot(212)
-    n, bins, patches = plt.hist(sa, 1000, range=(sa_mu-3*sa_sigma,sa_mu+3*sa_sigma),alpha=0.75)
-    plt.title('Parameter Sa')
-    plt.subplots_adjust(hspace=0.3)
-    plt.savefig(r"/home/anotherone/文档/mycode/Ca&Sa.svg",format="svg")
+    
+    fig,ax = plt.subplots(2,1,dpi=300)
+    n, bins, patches = ax[0].hist(ca, 1000, range=(ca_mu-3*ca_sigma,ca_mu+3*ca_sigma),color = 'g',alpha=0.75)
+    ax[0].set_title('Parameter Ca')
+    ax[0].set_xlabel("range")
+    ax[0].set_ylabel("amount")
+
+    n, bins, patches = ax[1].hist(sa, 1000, range=(sa_mu-3*sa_sigma,sa_mu+3*sa_sigma),alpha=0.75)
+    ax[1].set_title('Parameter Sa')
+    ax[1].set_xlabel("range", )
+    ax[1].set_ylabel("amount", )
+    plt.subplots_adjust(hspace=0.5)
+    #plt.savefig(r"/home/anotherone/文档/mycode/Ca&Sa.svg",format="svg")
     # plt.savefig(r"C:\Users\ironman\Desktop\idea\论文\使用小波分解重构法计算地震计安装方位角\英文投稿\figures\Ca&Sa.jpg",format="jpg")
+    plt.tight_layout()
     plt.show()
 
 # Illustrate the effect of proposed method by different level
 def illustration(ca,sa, level= None):
     max_level = pywt.dwt_max_level(len(ca),2)
     
-    plt.figure("illustration")
+    fig = plt.figure("illustration",dpi=300)
+    
     if level==None:
         level = max_level
-        plt.suptitle("Decomposition level: max",size = 16, color= "blue")
+        # plt.suptitle("Decomposition level: max",size = 16, color= "blue")
         # plt.text(ca_mu-4*ca_sigma, np.max(n_1), "wavedeposition level: max",size = 5, color= "blue",\
         # bbox=dict(boxstyle="round",ec=(1., 0.5, 0.5),fc=(1., 0.8, 0.8),))
     elif level>max_level :
         level = max_level
         print("Decomposition level is out of range! Setting to default.")
-        plt.suptitle("Decomposition level: "+str(level),size = 16, color= "blue")
+        # plt.suptitle("Decomposition level: "+str(level),size = 16, color= "blue")
     else:
-        plt.suptitle("Decomposition level: "+str(level),size = 16, color= "blue")
+        pass
+        # plt.suptitle("Decomposition level: "+str(level),size = 16, color= "blue")
         # plt.text(ca_mu-4*ca_sigma, np.max(n_1), "wavedeposition level: "+str(level), size = 5, color= "blue",\
         # bbox=dict(boxstyle="round",ec=(1., 0.5, 0.5),fc=(1., 0.8, 0.8),))
     rec_ca,rec_sa = r_rebuild(ca, sa, level=level)
@@ -215,64 +223,69 @@ def illustration(ca,sa, level= None):
     ca_sigma = np.std(ca, ddof =1)
     sa_mu = np.mean(sa)
     sa_sigma = np.std(sa, ddof =1)
-    plt.subplot(1,2,1)
-    n_1, bins_1, patches_1 = plt.hist(ca,bins = 1000,range=(ca_mu-6*ca_sigma,ca_mu+6*ca_sigma),log= True)
-    plt.ylabel("Log")
+    
+    ax1 = fig.add_subplot(1,2,1)
+    n_1, bins_1, patches_1 = ax1.hist(ca,bins = 1000,range=(ca_mu-6*ca_sigma,ca_mu+6*ca_sigma),)
+    ##   plt.ylabel("Log",fontsize=16)
     
     rec_ca_mu = np.mean(rec_ca)
     rec_ca_sigma = np.std(rec_ca, ddof =1)
     rec_sa_mu = np.mean(rec_sa)
     rec_sa_sigma = np.std(rec_sa, ddof =1)
-    n_2, bins_2, patches_2 = plt.hist(rec_ca,bins = 1000,range=(ca_mu-6*ca_sigma,ca_mu+6*ca_sigma),log= True,)
-    plt.legend(("Original histogram","Processed histogram "))
+    n_2, bins_2, patches_2 = ax1.hist(rec_ca,bins = 1000,range=(ca_mu-6*ca_sigma,ca_mu+6*ca_sigma),alpha=0.75,)
+    ax1.legend(("Original histogram","Processed histogram "),loc="upper right")
+    plt.xticks(fontsize=8)
+    plt.yticks(fontsize=8)
     
-    plt.subplot(1,2,2)
-    plt.plot(ca)
-    plt.plot(rec_ca)
-    plt.legend(("Before processing","After processing"))
-    
-    plt.subplots_adjust(left=0.05, right=0.98, top=0.9, bottom=0.05,wspace=0.1)
+    ax2 = fig.add_subplot(1,2,2)
+    ax2.plot(ca,linewidth=0.8)
+    ax2.plot(rec_ca,linewidth=0.8)
+    ax2.legend(("Before processing","After processing"),loc="upper right")
+    plt.xticks(fontsize=8,rotation=-30)
+    plt.yticks(fontsize=8)
+    ##    plt.subplots_adjust(left=0.05, right=0.98, top=0.9, bottom=0.05,wspace=0.1)
+    plt.tight_layout()
     plt.show()
 
 # Show the comparison 
 def effect(BHE,BHN,BLE,BLN,new_bhe,new_bhn):
     # Note: select 1000 points for illustrate the detial
-    fig = plt.figure("Raw data of E-W component")
+    fig = plt.figure("Raw data of E-W component",dpi=300)
     axs = fig.subplots()
     axs.set_title("Raw data BHE & BLE")
     axs.set_ylabel("count")
     axs.grid(True)
-    axs.plot((BHE.data[-1000:]-np.mean(BHE.data[-1000:])))
-    axs.plot(((BLE.data[-1000:]-np.mean(BLE.data[-1000:]))*4))
+    axs.plot((BHE.data[-1000:]-np.mean(BHE.data[-1000:])),linewidth=0.8)
+    axs.plot(((BLE.data[-1000:]-np.mean(BLE.data[-1000:]))*4),linewidth=0.8)
     axs.legend(("BHE","BLE"),loc="upper left")
     
-    fig = plt.figure("Raw data of N-S component")
+    fig = plt.figure("Raw data of N-S component",dpi=300)
     axs = fig.subplots()
-    axs.set_title("Raw data BHN & BLN")
+    axs.set_title("Raw data BHN & BLN",)
     axs.set_ylabel("count")
     axs.grid(True)
-    axs.plot((BHN.data[-1000:]-np.mean(BHN.data[-1000:])))
-    axs.plot(((BLN.data[-1000:]-np.mean(BLN.data[-1000:]))*4))
+    axs.plot((BHN.data[-1000:]-np.mean(BHN.data[-1000:])),linewidth=0.8)
+    axs.plot(((BLN.data[-1000:]-np.mean(BLN.data[-1000:]))*4),linewidth=0.8)
     axs.legend(("BHN","BLN"),loc="upper left")
 
-    fig = plt.figure("Comparison of corrected BHE data and raw BLE data")
+    fig = plt.figure("Comparison of corrected BHE data and raw BLE data",dpi=300)
     axs = fig.subplots()
-    axs.set_title("Corrected BHE data VS Raw BLE data")
-    axs.set_ylabel("count")
+    axs.set_title("Corrected BHE data VS Raw BLE data",)
+    axs.set_ylabel("count",)
     axs.grid(True)
-    axs.plot((new_bhe.data[-1000:]-np.mean(new_bhe.data[-1000:])))
-    axs.plot(((BLE.data[-1000:]-np.mean(BLE.data[-1000:]))*4))
-    axs.legend(("NEW_BHE","BLE"),loc="upper left")
+    axs.plot((new_bhe.data[-1000:]-np.mean(new_bhe.data[-1000:])),linewidth=0.8)
+    axs.plot(((BLE.data[-1000:]-np.mean(BLE.data[-1000:]))*4),linewidth=0.8)
+    axs.legend(("NEW_BHE","BLE"),loc="upper left",)
 
-    fig = plt.figure("Comparison of corrected BHN data and raw BLN data")
+    fig = plt.figure("Comparison of corrected BHN data and raw BLN data",dpi=300)
     axs = fig.subplots()
     axs.grid(True)
-    axs.set_title("Corrected BHN data VS Raw BLN data")
-    axs.set_ylabel("count")
-    axs.plot((new_bhn.data[-1000:]-np.mean(new_bhn.data[-1000:])))
-    axs.plot(((BLN.data[-1000:]-np.mean(BLN.data[-1000:]))*4))
+    axs.set_title("Corrected BHN data VS Raw BLN data",)
+    axs.set_ylabel("count",)
+    axs.plot((new_bhn.data[-1000:]-np.mean(new_bhn.data[-1000:])),linewidth=0.8)
+    axs.plot(((BLN.data[-1000:]-np.mean(BLN.data[-1000:]))*4),linewidth=0.8)
     axs.legend(("NEW_BHN","BLN"),loc="upper left")
-    
+    plt.tight_layout()
     plt.show()
 
 # Furthermore display
@@ -282,22 +295,22 @@ def additional_effect(BLE,BLN,new_bhe,new_bhn):
     BLE = BLE.filter("bandpass",freqmin=0.1,freqmax=0.5)
     BLN = BLN.filter("bandpass",freqmin=0.1,freqmax=0.5)
 
-    fig= plt.figure("Comparison of corrected BHE data and raw BLE data after filter")
+    fig= plt.figure("Comparison of corrected BHE data and raw BLE data after filter",dpi=300)
     axs = fig.subplots()
     axs.grid(True)
-    axs.set_title("filtered data :NEW_BHE_flitered & BLE_filtered ")
-    axs.plot((new_bhe.data[-1000:]-np.mean(new_bhe.data[-1000:])),'x')
-    axs.plot(((BLE.data[-1000:]-np.mean(BLE.data[-1000:]))*4))
-    axs.legend(("NEW_BHE_FILTERED","BLE_FILTERED"),loc="upper left")
+    axs.set_title("filtered data :NEW_BHE_flitered & BLE_filtered ",)
+    axs.plot((new_bhe.data[-1000:]-np.mean(new_bhe.data[-1000:])),'x',linewidth=0.8)
+    axs.plot(((BLE.data[-1000:]-np.mean(BLE.data[-1000:]))*4),linewidth=0.8)
+    axs.legend(("NEW_BHE_FILTERED","BLE_FILTERED"),loc="upper left",)
     
-    fig = plt.figure("Comparison of corrected BHN data and raw BLN data after filter")
+    fig = plt.figure("Comparison of corrected BHN data and raw BLN data after filter",dpi=300)
     axs = fig.subplots()
     axs.grid(True)
-    axs.set_title("filtered data :NEW_BHN_flitered & BLN_filtered ")
-    axs.plot((new_bhn.data[-1000:]-np.mean(new_bhn.data)),'x')
-    axs.plot(((BLN.data[-1000:]-np.mean(BLN.data[-1000:]))*4))
-    axs.legend(("NEW_BHN_FILTERED","BLN_FILTERED"),loc="upper left")
-
+    axs.set_title("filtered data :NEW_BHN_flitered & BLN_filtered ",)
+    axs.plot((new_bhn.data[-1000:]-np.mean(new_bhn.data)),'x',linewidth=0.8)
+    axs.plot(((BLN.data[-1000:]-np.mean(BLN.data[-1000:]))*4),linewidth=0.8)
+    axs.legend(("NEW_BHN_FILTERED","BLN_FILTERED"),loc="upper left",)
+    plt.tight_layout()
     plt.show()
 
 # Calculate the Root Mean Squared Error (RMSE)
@@ -344,14 +357,15 @@ if __name__ == '__main__':
     effect(BHE,BHN,BLE,BLN,new_bhe,new_bhn)
     additional_effect(BLE,BLN,new_bhe,new_bhn)
     plothist(ca,sa)
-    illustration(ca,sa)
-##    
-##s_1,s_2=[],[]
-##for n in range(100):
-##    s_1.append(np.mean(random.sample(list(ca),10)))
-##    s_2.append(np.mean(random.sample(list(rec_ca),10)))
-##plt.figure("100 tests")
-##plt.plot(s_1,"X",s_2,".")
-##plt.title("Results of 100 tests")
-##plt.legend(("Average of raw data sample","Average of processed data sample"))
-##plt.show()
+##    illustration(ca,sa)
+    
+    s_1,s_2=[],[]
+    for n in range(100):
+        s_1.append(np.mean(random.sample(list(ca),10)))
+        s_2.append(np.mean(random.sample(list(rec_ca),10)))
+    plt.figure("100 tests",dpi=300)
+    plt.plot(s_1,"x",s_2,".",)
+    plt.title("Results of 100 tests",)
+    plt.legend(("Average of raw data sample","Average of processed data sample"),)
+    plt.tight_layout()
+    plt.show()
